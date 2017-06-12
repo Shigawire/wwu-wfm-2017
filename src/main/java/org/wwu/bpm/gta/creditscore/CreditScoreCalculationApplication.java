0d90ee47b03879bff2540df1fccb73e196a576b5
Fabian Kübler <fabian-kuebler@t-online.de>
2017-06-12 15:20:41 +0200

    use decorator

--- src/main/java/org/wwu/bpm/gta/creditscore/CreditScoreCalculationApplication.java
+++ src/main/java/org/wwu/bpm/gta/creditscore/CreditScoreCalculationApplication.java
@@ -1,5 +1,9 @@
 package org.wwu.bpm.gta.creditscore;
 
-public class CreditScoreCalculationApplication {
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
 
+@ProcessApplication("Credit Score Calculation App")
+public class CreditScoreCalculationApplication extends ServletProcessApplication {
+  // empty implementation
 }
