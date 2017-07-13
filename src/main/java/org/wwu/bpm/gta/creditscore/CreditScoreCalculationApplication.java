bc4fce572379e3d607e456113249d8dcb28f44f2
Fabian Kübler <fabian-kuebler@t-online.de>
2017-07-13 16:20:42 +0200

    add java Delegate to classes

--- src/main/java/org/wwu/bpm/gta/creditscore/CreditScoreCalculationApplication.java
+++ src/main/java/org/wwu/bpm/gta/creditscore/CreditScoreCalculationApplication.java
@@ -2,8 +2,16 @@ package org.wwu.bpm.gta.creditscore;
 
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 @ProcessApplication("Credit Score Calculation App")
-public class CreditScoreCalculationApplication extends ServletProcessApplication {
+public class CreditScoreCalculationApplication extends ServletProcessApplication implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
   // empty implementation
 }
--- src/main/java/org/wwu/bpm/gta/creditscore/SendCreditScoreReport.java
+++ src/main/java/org/wwu/bpm/gta/creditscore/SendCreditScoreReport.java
@@ -1,7 +1,15 @@
 package org.wwu.bpm.gta.creditscore;
 
 import org.camunda.bpm.application.impl.ServletProcessApplication;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-public class SendCreditScoreReport extends ServletProcessApplication {
+public class SendCreditScoreReport extends ServletProcessApplication implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
