7f55b03f8d88685c0622ad347976f4212437a242
Fabian Kübler <fabian-kuebler@t-online.de>
2017-07-14 08:39:42 +0200

    add Lukas MySQL stuff

--- src/main/java/org/wwu/bpm/gta/creditscore/CheckApplicantExists.java
+++ src/main/java/org/wwu/bpm/gta/creditscore/CheckApplicantExists.java
@@ -3,7 +3,11 @@ package org.wwu.bpm.gta.creditscore;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 //@ProcessApplication("Credit Score Calculation App")
 public class CheckApplicantExists extends ServletProcessApplication implements JavaDelegate {
@@ -13,4 +17,66 @@ public class CheckApplicantExists extends ServletProcessApplication implements J
 		// TODO Auto-generated method stub
 		
 	}
+	
+	
+	//if the applicant exists, get ID, date of last checking, score of last checking. 
+	//need to calculate the duration between last check and current date then. 
+	//duration is better to be named as "durationDay". (Because it's used in decision table)
+	
+	
+		public static void main (String[] args) {
+			String firstname = "";
+			String lastname = "";
+			String birthDate = "";
+			
+			
+			Connection con = connectDatabase();
+			boolean exists = checkApplicant (con, firstname, lastname, birthDate);
+			
+			if (exists) {
+				// trigger check record
+			} else {
+				// trigger create new account calling a web form to insert applicant data
+			}
+			
+		}
+		
+		public static Connection connectDatabase () {
+			String url = ""; //enter database url
+			String username = ""; //enter database username
+			String password = ""; //enter database password
+			
+			System.out.println("Connection database...");
+			
+			try (Connection connection = DriverManager.getConnection(url, username, password)){
+					System.out.println("Database Connected!");
+					return connection;
+			} catch (SQLException e) {
+				throw new IllegalStateException ("Connot connect to database!", e);
+			}
+			
+		}
+
+		public static boolean checkApplicant(Connection con, String firstname, String lastname, String birthDate) {
+			boolean exists = false;
+			PreparedStatement ps;
+			ResultSet rs;
+			
+			try {
+				ps = con.prepareStatement("SELECT EXISTS(Select * FROM customer WHERE firstname = ? AND lastname = ? AND birthdate = ?" );
+				ps.setObject(0, firstname);
+				ps.setObject(1, lastname);
+				ps.setObject(2, birthDate);
+				rs = ps.executeQuery();
+				if (rs == null) 
+					exists = true;
+				else exists = false;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return exists;
+			
+			
+		}
 }
--- src/main/java/org/wwu/bpm/gta/creditscore/GenerateInvoice.java
+++ src/main/java/org/wwu/bpm/gta/creditscore/GenerateInvoice.java
@@ -1,14 +1,21 @@
 package org.wwu.bpm.gta.creditscore;
 
+import java.util.logging.Logger;
+
 import org.camunda.bpm.application.impl.ServletProcessApplication;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 public class GenerateInvoice extends ServletProcessApplication  implements JavaDelegate {
-
+	
+	double CREDIT_SCORE_CHECK_PRICE = 0.75;
+	private final static Logger LOGGER = Logger.getLogger("GTA");
+	
 	@Override
-	public void execute(DelegateExecution arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		int investigationVolume = (int) execution.getVariable("investigationVolume");
+		LOGGER.info("YOLO, received volume:" + investigationVolume);
 		
 	}
  
