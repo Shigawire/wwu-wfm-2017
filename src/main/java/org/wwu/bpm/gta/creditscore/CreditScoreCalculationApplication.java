977fe4d0fefecff197e0ba326012766f3c1fd63e
WenyingDing <deliad19@gmail.com>
2017-07-14 09:46:20 +0200

    edit CheckRecord decision table and add codes for running it with an instance

--- src/main/java/org/wwu/bpm/gta/creditscore/CreditScoreCalculationApplication.java
+++ src/main/java/org/wwu/bpm/gta/creditscore/CreditScoreCalculationApplication.java
@@ -1,9 +1,32 @@
 package org.wwu.bpm.gta.creditscore;
 
+import org.camunda.bpm.application.PostDeploy;
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
+import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
+import org.camunda.bpm.engine.DecisionService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
 
-@ProcessApplication("Credit Score Calculation App")
+
+@ProcessApplication("Credit Score Calculation")
+
 public class CreditScoreCalculationApplication extends ServletProcessApplication {
-  // empty implementation
+
+  @PostDeploy
+  public void evaluateDecisionTable(ProcessEngine processEngine) {
+
+    DecisionService decisionService = processEngine.getDecisionService();
+
+    VariableMap variables = Variables.createVariables()
+      .putValue("durationDay", 100)
+      .putValue("creditScore", 8);
+
+    DmnDecisionTableResult CheckRecord = decisionService.evaluateDecisionTableByKey("CheckRecord", variables);
+    String investigationNecessity = CheckRecord.getSingleEntry();
+
+    System.out.println("Is it necessary to investigate: " + investigationNecessity);
+  }
+
 }
