b3b280cf1affc191b80c15f732efbbf66e04de2a
Lukas Zimmermann <lukas.zimmermann@paderborn.com>
2017-07-18 19:02:52 +0200

    finished Class Applicant

--- src/main/java/org/wwu/bpm/gta/creditscore/Applicant.java
+++ src/main/java/org/wwu/bpm/gta/creditscore/Applicant.java
@@ -6,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 import org.camunda.bpm.application.impl.ServletProcessApplication;
